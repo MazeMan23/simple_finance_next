cab76de20d3afd7b504339bd1729eb914b9190b2
Ognian Trajanov Jr <ogtrjr@gmail.com>
2022-09-27 17:57:22 -0400

    regardation

--- pages/finance.js
+++ pages/finance.js
@@ -6,10 +6,14 @@ import { Collapse, Text, Grid, Avatar, Card, Row } from "@nextui-org/react";
 import ContactForm from "../components/ContactForm";
 
 export async function getStaticProps({ locale }) {
-  const index = (await import(`../translations/finance/${locale}.json`)).default;
-  const header = (await import(`../translations/header/${locale}.json`)).default;
-  const footer = (await import(`../translations/footer/${locale}.json`)).default;
-  const contact = (await import(`../translations/contact/${locale}.json`)).default;
+  const index = (await import(`../translations/finance/${locale}.json`))
+    .default;
+  const header = (await import(`../translations/header/${locale}.json`))
+    .default;
+  const footer = (await import(`../translations/footer/${locale}.json`))
+    .default;
+  const contact = (await import(`../translations/contact/${locale}.json`))
+    .default;
 
   const final = { ...index, ...header, ...footer, ...contact };
 
@@ -61,20 +65,31 @@ export default function Finance() {
     <Layout h={h} f={f}>
       <ParticlesHero img="/images/simple_finance.png">
         <div className="flex flex-col justify-center text-center text-white gap-4">
-          <div className=" text-2xl md:text-5xl font-bold">{t("heroTitle")}</div>
-          <div className="text-xl md:text-4xl font-bold">{t("heroSubtitle1")}</div>
-          <div className="text-lg md:text-xl font-semibold tracking-widest">{t("heroSubtitle2")}</div>
+          <div className=" text-2xl md:text-5xl font-bold">
+            {t("heroTitle")}
+          </div>
+          <div className="text-xl md:text-4xl font-bold">
+            {t("heroSubtitle1")}
+          </div>
+          <div className="text-lg md:text-xl font-semibold tracking-widest">
+            {t("heroSubtitle2")}
+          </div>
         </div>
       </ParticlesHero>
       <div className="flex flex-col items-center mt-8">
         <h1 className="text-2xl font-semibold">{t("servicesTitle")}</h1>
-        <p className=" mt-2 text-center lg:ml-64 lg:mr-64">{t("serviceSubtitle")}</p>
+        <p className=" mt-2 text-center lg:ml-64 lg:mr-64">
+          {t("serviceSubtitle")}
+        </p>
         <div className="flex flex-col lg:flex-row">
           <div className="flex flex-1">
             <Grid.Container gap={2}>
               <Grid>
                 <Collapse.Group splitted>
-                  <Collapse title={t("accounting_title")} contentLeft={<Avatar src="/images/acc_av.jpg" size="xl" />}>
+                  <Collapse
+                    title={t("accounting_title")}
+                    contentLeft={<Avatar src="/images/acc_av.jpg" size="xl" />}
+                  >
                     <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
                       <li className="mt-1">{t("accounting2")}</li>
                       <li className="mt-1">{t("accounting3")}</li>
@@ -86,7 +101,10 @@ export default function Finance() {
                       <li className="mt-1">{t("accounting1")}</li>
                     </ul>
                   </Collapse>
-                  <Collapse title={t("financial_title")} contentLeft={<Avatar src="/images/jws.jpg" size="xl" />}>
+                  <Collapse
+                    title={t("financial_title")}
+                    contentLeft={<Avatar src="/images/jws.jpg" size="xl" />}
+                  >
                     <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
                       <li className="mt-1">{t("financial1")}</li>
                       <li className="mt-1">{t("financial2")}</li>
@@ -98,7 +116,12 @@ export default function Finance() {
                       <li className="mt-1">{t("financial8")}</li>
                     </ul>
                   </Collapse>
-                  <Collapse title={t("legal_title")} contentLeft={<Avatar src="/images/jwstreet.jpg" size="xl" />}>
+                  <Collapse
+                    title={t("legal_title")}
+                    contentLeft={
+                      <Avatar src="/images/jwstreet.jpg" size="xl" />
+                    }
+                  >
                     <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
                       <li className="mt-1">{t("legal1")}</li>
                       <li className="mt-1">{t("legal2")}</li>
@@ -110,7 +133,10 @@ export default function Finance() {
                       <li className="mt-1">{t("legal8")}</li>
                     </ul>
                   </Collapse>
-                  <Collapse contentLeft={<Avatar src="/images/taxes.jpg" size="xl" />} title={t("tax_title")}>
+                  <Collapse
+                    contentLeft={<Avatar src="/images/taxes.jpg" size="xl" />}
+                    title={t("tax_title")}
+                  >
                     <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
                       <li className="mt-1">{t("tax1")}</li>
                       <li className="mt-1">{t("tax2")}</li>
@@ -130,7 +156,9 @@ export default function Finance() {
                 <Collapse.Group splitted>
                   <Collapse
                     title={t("business_val_title")}
-                    contentLeft={<Avatar src="/images/valuation.jpg" size="xl" />}
+                    contentLeft={
+                      <Avatar src="/images/valuation.jpg" size="xl" />
+                    }
                   >
                     <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
                       <li className="mt-1">{t("business_val1")}</li>
@@ -142,23 +170,27 @@ export default function Finance() {
                       <li className="mt-1">{t("business_val7")}</li>
                     </ul>
                   </Collapse>
-                  <Collapse title={t("audit")} contentLeft={<Avatar src="/images/audit.jpg" size="xl" />}>
+                  <Collapse
+                    title={t("audit")}
+                    contentLeft={<Avatar src="/images/audit.jpg" size="xl" />}
+                  >
                     <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
                       <li className="mt-1">{t("audit1")}</li>
                       <li className="mt-1">{t("audit2")}</li>
                       <li className="mt-1">{t("audit3")}</li>
                     </ul>
                   </Collapse>
-                  <Collapse title={t("hr")} contentLeft={<Avatar src="/images/ppl.jpg" size="xl" />}>
-                    <Text>
-                      <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
-                        <li className="mt-1">{t("hr1")}</li>
-                        <li className="mt-1">{t("hr2")}</li>
-                        <li className="mt-1">{t("hr3")}</li>
-                        <li className="mt-1">{t("hr4")}</li>
-                        <li className="mt-1">{t("hr5")}</li>
-                      </ul>
-                    </Text>
+                  <Collapse
+                    title={t("hr")}
+                    contentLeft={<Avatar src="/images/ppl.jpg" size="xl" />}
+                  >
+                    <ul className="list-disc ml-5 mt-5 marker:text-orange-500">
+                      <li className="mt-1">{t("hr1")}</li>
+                      <li className="mt-1">{t("hr2")}</li>
+                      <li className="mt-1">{t("hr3")}</li>
+                      <li className="mt-1">{t("hr4")}</li>
+                      <li className="mt-1">{t("hr5")}</li>
+                    </ul>
                   </Collapse>
                 </Collapse.Group>
               </Grid>
@@ -173,16 +205,27 @@ export default function Finance() {
               <Grid xs={16} sm={3} key={index}>
                 <Card isPressable>
                   <Card.Body css={{ p: 0 }}>
-                    <Card.Image src={item.banner} objectFit="cover" width="100%" height={300} alt={item.url} />
+                    <Card.Image
+                      src={item.banner}
+                      objectFit="cover"
+                      width="100%"
+                      height={300}
+                      alt={item.url}
+                    />
                     <div className="flex flex-row ml-3 mt-3">
                       <div className="flex flex-col">
-                        <Text className="text-lg font-semibold">{item.category}</Text>
+                        <p className="text-lg font-semibold">{item.category}</p>
                       </div>
                     </div>
                   </Card.Body>
                   <Card.Footer css={{ justifyItems: "flex-start" }}>
-                    <Row wrap="wrap" justify="space-between" align="center" className=" items-center">
-                      <Text
+                    <Row
+                      wrap="wrap"
+                      justify="space-between"
+                      align="center"
+                      className=" items-center"
+                    >
+                      <p
                         css={{
                           color: "$accents7",
                           fontWeight: "$semibold",
@@ -194,7 +237,7 @@ export default function Finance() {
                           <li className="mt-1">{item.desc2}</li>
                           <li className="mt-1">{item.desc3}</li>
                         </ul>
-                      </Text>
+                      </p>
                     </Row>
                   </Card.Footer>
                 </Card>
