653c64d08530466c9f9e55c723c823e57acdea01
NikiHadzhi <niki.hadzhi@gmail.com>
2023-04-06 19:17:13 +0300

    Working on simple-hr page

--- pages/hr.js
+++ pages/hr.js
@@ -4,6 +4,8 @@ import Layout from "../components/Layout";
 import ParticlesHero from "../components/ParticlesHero";
 import ContactForm from "../components/ContactForm";
 import Image from "next/image";
+import { Collapse } from "@nextui-org/react";
+
 
 export async function getStaticProps({ locale }) {
   const simpleHr = (await import(`../translations/hr/${locale}.json`))
@@ -35,32 +37,54 @@ export default function Finance() {
     {
       category: t("staffSearch_title"),
       banner: "/images/staff-search.jpg",
-      info: t("search"),
+      desc: t("staffSearch_desc"),
     },
     {
       category: t("staffLeasing_title"),
       banner: "/images/staff-leasing.jpg",
-      info: t("leasing"),
+      desc: t("leasing_desc"),
+      info: [t("leasing_info1"), t('leasing_info2')],
+      args: [t('leasing_info1_arg1'), t('leasing_info1_arg2'), t('leasing_info1_arg3'), t('leasing_info1_arg4')],
+      args1: [t('leasing_info1_arg5'), t('leasing_info1_arg6'), t('leasing_info1_arg7'), t('leasing_info1_arg8')]
     },
     {
       category: t("staffEvaluation_title"),
       banner: "/images/staff-evaluation.jpg",
-      info: t("evaluation"),
+      desc: t('staffEvaluation_desc')
     },
     {
       category: t("marketResearch_title"),
       banner: "/images/market-research.jpg",
-      info: t("market"),
+      desc: t('marketResearch_desc')
     },
     {
       category: t("staffAdministration_title"),
       banner: "/images/staff-administration.jpg",
-      info: t("administration"),
+      desc: t('staffAdministration_desc'),
+      info: [t("staffAdministration_info")],
+      args: [t('administration_arg1'),
+      t('administration_arg2'),
+      t('administration_arg3'),
+      t('administration_arg4'),
+      t('administration_arg5'),
+      t('administration_arg6'),
+      t('administration_arg7'),
+      t('administration_arg8'),
+      t('administration_arg9'),
+      t('administration_arg10'),
+      ]
     },
     {
       category: t("compStructuring_title"),
       banner: "/images/comp-structuring.jpg",
-      info: t("structuring"),
+      info: [t("compStructuring_desc")],
+      args: [t('compStructuring_arg1'),
+      t('compStructuring_arg2'),
+      t('compStructuring_arg3'),
+      t('compStructuring_arg4'),
+      t('compStructuring_arg5'),
+      t('compStructuring_arg6'),
+      t('compStructuring_arg7'),]
     },
   ];
 
@@ -77,74 +101,62 @@ export default function Finance() {
           </div>
         </div>
       </ParticlesHero>
-      <h1 className="flex justify-center text-4xl font-bold mt-10 my-12 text-orange-400">
+      <h1 className="flex justify-center text-4xl font-bold mt-10 my-12">
         {t("our_works")}
       </h1>
-      <div className="flex flex-col items-center justify-center flex-wrap gap-5 px-5 mb-10 lg:flex-row ">
-        {services_list.map((item, index) =>
-          index % 2 === 0 ? (
+      <Collapse.Group>
+        <div className="flex flex-row justify-center gap-10 px-8 mb-8 flex-wrap">
+          {services_list.map((item, index) => (
             <div
               key={index}
-              className="flex justify-center rounded-xl bg-white shadow-xl w-[82%] lg:w-[76%] xl:w-[49%] xl:h-[396px]"
+              className="rounded-xl bg-white active:bg-gray-200 w-full md:w-[68%] lg:w-[43%]"
             >
-              <div className="flex max-w-[378px]">
+              <div className="flex flex-col">
                 <Image
-                  className="rounded-l-lg object-cover"
                   src={item.banner}
+                  className="rounded-t-xl"
                   layout="intrinsic"
                   width={412}
-                  height={320}
+                  height={360}
                 />
               </div>
-              <div className="mx-auto max-w-[48%] my-6">
-                <div className="flex flex-col justify-center">
-                  <h1 className="font-bold text-2xl text-center">
-                    {item.category}
-                  </h1>
-                  <p className="my-6 mx-4 text-justify font-semibold">{item.info}</p>
-                  {/* <ul className="mx-8 mb-2 mt-3 list-disc text-gray-700 font-semibold text-smml-5 marker:text-orange-500">
-                    {item.info.map((x, index) => (
-                      <li className="mt-1" key={index}>
-                        {x}
-                      </li>
-                    ))}
-                  </ul> */}
-                </div>
-              </div>
+              <Collapse
+                className="font-bold text-2xl"
+                title={item.category}
+                shadow='true'
+              >
+                <p className="indent-5 font-bold text-gray-700 text-left">{item.desc}</p>
+                {item['info'] && item['info'].length > 1
+                  ? <div>
+                    <p className="indent-5 font-bold text-gray-700">{item.info[0]}</p>
+                    <ul className="font-semibold text-gray-700 list-disc marker: marker:text-orange-500">
+                      {item.args.map((arg, index) => (
+                        <li className="ml-12" key={index}>{arg}</li>
+                      ))}
+                    </ul>
+                    <p className="indent-5 font-bold text-gray-700">{item.info[1]}</p>
+                    <ul className="font-semibold text-gray-700 list-disc marker: marker:text-orange-500">
+                      {item.args.map((arg, index) => (
+                        <li className="ml-12" key={index}>{arg}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  : item['args']
+                    ? <div>
+                      <p className="indent-5 font-bold text-gray-700">{item.info[0]}</p>
+                      <ul className="font-semibold text-gray-700 list-disc marker: marker:text-orange-500">
+                        {item.args.map((arg, index) => (
+                          <li className="ml-12" key={index}>{arg}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    : <></>
+                }
+              </Collapse>
             </div>
-          ) : (
-            <div
-              key={index}
-              className="flex justify-between rounded-xl bg-white shadow-xl w-[82%] lg:w-[76%] xl:w-[49%] xl:h-[396px]"
-            >
-              <div className="mx-auto max-w-[48%] my-6">
-                <div className="flex flex-col justify-center">
-                  <h1 className="font-bold text-2xl text-center">
-                    {item.category}
-                  </h1>
-                  <p className="my-6 mx-4 text-justify font-semibold">{item.info}</p>
-                  {/* <ul className="mx-8 mb-2 list-disc text-gray-700 font-semibold text-smml-5 mt-3 marker:text-orange-500">
-                    {item.info.map((x, index) => (
-                      <li className="mt-1" key={index}>
-                        {x}
-                      </li>
-                    ))}
-                  </ul> */}
-                </div>
-              </div>
-              <div className="flex max-w-[378px]">
-                <Image
-                  className="rounded-r-lg object-cover"
-                  src={item.banner}
-                  layout="intrinsic"
-                  width={412}
-                  height={320}
-                />
-              </div>
-            </div>
-          )
-        )}
-      </div>
+          ))}
+        </div>
+      </Collapse.Group>
       <ContactForm t={c} h={h} />
     </Layout>
   );
