8df64e7551135634106acd4bd34408f67a6d040c
NikiHadzhi <niki.hadzhi@gmail.com>
2023-04-11 07:23:10 +0300

    Update information of Standarts page

--- pages/standarts.js
+++ pages/standarts.js
@@ -32,42 +32,87 @@ export default function Finance() {
     our_services: [t("our_service_desc"), t("our_service_desc1"), t("our_service_desc2")],
   }
 
-  const services_list = [
+  const certificates_list = [
     {
       category: t("title_9001"),
       banner: "/images/ISO_9001-2015.jpg",
       desc: t("9001_desc"),
+      benefit: t('9001_benefit'),
+      args: [t('9001_arg'), t('9001_arg1'), t('9001_arg2'),]
     },
     {
       category: t("title_14001"),
       banner: "/images/ISO-14001.png",
-      desc: t("leasing_desc"),
-      info: [t("leasing_info1"), t("leasing_info2")],
-      args: [t("leasing_info1_arg1"), t("leasing_info1_arg2"), t("leasing_info1_arg3"), t("leasing_info1_arg4")],
-      args1: [t("leasing_info1_arg5"), t("leasing_info1_arg6"), t("leasing_info1_arg7"), t("leasing_info1_arg8")],
+      desc: t("14001_desc"),
+      benefit: t('14001_benefit'),
+      args: [
+        t('14001_arg'),
+        t('14001_arg1'),
+        t('14001_arg2'),
+        t('14001_arg3'),
+        t('14001_arg4'),
+        t('14001_arg5'),
+        t('14001_arg6'),
+        t('14001_arg7'),
+        t('14001_arg8'),
+        t('14001_arg9'),
+        t('14001_arg10'),
+        t('14001_arg11'),
+      ]
     },
     {
       category: t("title_45001"),
       banner: "/images/Iso-45001.png",
-      desc: t("staffEvaluation_desc"),
+      desc: t("45001_desc"),
+      benefit: t('45001_benefit'),
+      args: [
+        t('45001_arg'),
+        t('45001_arg1'),
+        t('45001_arg2'),
+        t('45001_arg3'),
+        t('45001_arg4'),
+      ],
+      info: t('45001_addit')
     },
     {
       category: t("title_20000"),
       banner: "/images/ISO-20000.png",
-      desc: t("marketResearch_desc"),
+      desc: t("20000_desc"),
     },
     {
       category: t("title_27001"),
       banner: "/images/ISO-27001.png",
-      desc: t("staffAdministration_desc"),
-      info: [t("staffAdministration_info")],
-      args: [t("administration_arg1"), t("administration_arg2"), t("administration_arg3"), t("administration_arg4"), t("administration_arg5"), t("administration_arg6"), t("administration_arg7"), t("administration_arg8"), t("administration_arg9"), t("administration_arg10")],
+      desc: t("27001_desc"),
+      benefit: t('27001_benefit'),
+      args: [
+        t("27001_arg"),
+        t("27001_arg1"),
+        t("27001_arg2"),
+        t("27001_arg3"),
+        t("27001_arg4"),
+        t("27001_arg5"),
+        t("27001_arg6")
+      ]
     },
     {
       category: t("title_39001"),
       banner: "/images/ISO-39001.png",
-      info: [t("compStructuring_desc")],
-      args: [t("compStructuring_arg1"), t("compStructuring_arg2"), t("compStructuring_arg3"), t("compStructuring_arg4"), t("compStructuring_arg5"), t("compStructuring_arg6"), t("compStructuring_arg7")],
+      benefit: t("39001_benefit"),
+      desc: t("39001_desc"),
+      args: [
+        t('39001_arg'),
+        t('39001_arg1'),
+        t('39001_arg2'),
+        t('39001_arg3'),
+        t('39001_arg4'),
+        t('39001_arg5'),
+        t('39001_arg6'),
+        t('39001_arg7'),
+        t('39001_arg8'),
+        t('39001_arg9'),
+        t('39001_arg10'),
+        t('39001_arg11'),
+      ]
     },
   ];
 
@@ -91,7 +136,7 @@ export default function Finance() {
       </div>
       <Collapse.Group>
         <div className="flex flex-row justify-center gap-10 px-8 mb-8 flex-wrap">
-          {services_list.map((item, index) => (
+          {certificates_list.map((item, index) => (
             <div key={index} className="rounded-xl bg-white active:bg-gray-200 w-full md:w-[68%] lg:w-[43%] xl:w-[41%] 2xl:w-[47%]">
               <Collapse
                 className="font-semibold text-xs md:text-xl scale-100 hover:scale-105"
@@ -100,40 +145,24 @@ export default function Finance() {
                 contentLeft={
                   <Image src={item.banner} className="rounded-t-xl" objectFit="cover" width={180} height={180} />}
               >
-                <p className="indent-5 font-bold text-gray-700 text-left">{item.desc}</p>
-                {item["info"] && item["info"].length > 1 ? (
+                <p className="mb-3 indent-5 font-bold text-gray-700 text-left">{item.desc}</p>
+                {item["benefit"] ? (
                   <div>
-                    <p className="indent-5 font-bold text-gray-700">{item.info[0]}</p>
+                    <p className="indent-5 font-bold text-gray-700">{item.benefit}</p>
                     <ul className="font-semibold text-gray-700 list-disc marker: marker:text-orange-500">
                       {item.args.map((arg, index) => (
-                        <li className="ml-12" key={index}>
+                        <li className="ml-10" key={index}>
                           {arg}
                         </li>
                       ))}
                     </ul>
-                    <p className="indent-5 font-bold text-gray-700">{item.info[1]}</p>
-                    <ul className="font-semibold text-gray-700 list-disc marker: marker:text-orange-500">
-                      {item.args.map((arg, index) => (
-                        <li className="ml-12" key={index}>
-                          {arg}
-                        </li>
-                      ))}
-                    </ul>
+                    {item["info"] ?
+                      <p className="mt-3 indent-5 font-bold text-gray-700">{item.info}</p>
+                      : <></>
+                    }
                   </div>
-                ) : item["args"] ? (
-                  <div>
-                    <p className="indent-5 font-bold text-gray-700">{item.info[0]}</p>
-                    <ul className="font-semibold text-gray-700 list-disc marker: marker:text-orange-500">
-                      {item.args.map((arg, index) => (
-                        <li className="ml-12" key={index}>
-                          {arg}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ) : (
-                  <></>
-                )}
+                ) : <></>
+                }
               </Collapse>
             </div>
           ))}
