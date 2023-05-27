cb1f7028594cab2e2856ce46d0f697a4893330d1
NikiHadzhi <niki.hadzhi@gmail.com>
2023-05-27 19:22:25 +0300

    Added some responsive design to Simple Offices

--- pages/offices.js
+++ pages/offices.js
@@ -54,7 +54,7 @@ export default function Translate() {
     <Layout h={h} f={f}>
       <ParticlesHero img="/images/offices.png">
         <div className="flex flex-col justify-center text-center items-center text-white gap-4">
-          <div className="h-48 w-48 self-center">
+          <div className="h-44 w-36 lg:h-48 lg:w-48 self-center">
             <Image
               src="/images/offices_logo.png"
               width={250}
@@ -62,18 +62,18 @@ export default function Translate() {
               alt="Simple Offices Logo"
             />
           </div>
-          <div className=" text-2xl md:text-4xl max-w-4xl font-bold tracking-tight">{t("heroTitle")}</div>
+          <div className=" text-2xl lg:text-4xl max-w-4xl font-bold tracking-tight">{t("heroTitle")}</div>
         </div>
       </ParticlesHero>
-      <div className="flex flex-col items-center text-xl px-[6%] pt-12 pb-6 gap-2 text-justify tracking-tight">
+      <div className="flex flex-col items-center text-lg lg:text-xl 2xl:text-2xl px-[6%] pt-12 pb-6 gap-2 text-justify tracking-tight">
         <div className="font-semibold">{t("textTitle")}</div>
         <div className="">{t("text")}</div>
         <div className="font-semibold">{t("finalText")}</div>
       </div>
 
-      <div className="grid grid-cols-2 gap-12 justify-center mx-[18%] my-16">
+      <div className="grid grid-cols-1 gap-10 justify-center mx-auto my-8 md:grid-cols-2 2xl:grid-cols-4">
         {offices_list.map((office, index) =>
-          <Card key={index}>
+          <Card key={index} className="w-[350px] lg:w-[420px]">
             <Card.Image
               src={office.img}
               objectFit="cover"
